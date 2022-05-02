44c46c7aba46077b187adbfd98ca4c094e361f90
Matias De Los Rios <94649472+MDA-2@users.noreply.github.com>
2022-05-02 17:31:43 -0500

    Update helper.sh

--- helper.sh
+++ helper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $1 == "-h" ]; then
-    printf "You can enter the name of a package to install it\nexample: ./helper.sh install package\n"
+    printf "You can enter the name of a package to install it\nexample: ./helper.sh -i package\n"
 
 elif [ $1 == "-i" ]; then
     git clone https://aur.archlinux.org/$2.git
