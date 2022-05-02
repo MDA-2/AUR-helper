dc55d5b224a0ce27ef9c0e70f251a9e5213a1288
Matias De Los Rios <94649472+MDA-2@users.noreply.github.com>
2022-05-02 17:27:43 -0500

    Add files via upload

---
+++ helper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $1 == "-h" ]; then
+    printf "You can enter the name of a package to install it\nexample: ./helper.sh install package\n"
+
+elif [ $1 == "-i" ]; then
+    git clone https://aur.archlinux.org/$2.git
+    cd $2
+    makepkg -si
+fi
+
