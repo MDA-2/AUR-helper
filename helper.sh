e86d6c15087fcddc9b0742c302437d76fadefc8a
Matias De Los Rios <94649472+MDA-2@users.noreply.github.com>
2022-07-06 12:21:17 -0500

    Delete helper.sh

--- helper.sh
+++
@@ -1,8 +0,0 @@
-if [ $1 == "-h" ]; then
-    printf "You can enter the name of a package to install it\nexample: ./helper.sh -i package\n"
-
-elif [ $1 == "-i" ]; then
-    git clone https://aur.archlinux.org/$2.git
-    cd $2
-    makepkg -si
-fi
